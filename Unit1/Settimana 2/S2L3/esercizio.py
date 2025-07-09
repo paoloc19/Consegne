edbcb9c278a07e976ddfe67aa9ebb16913fb0fb3
Paolo Costanzo <paco.costanzo@gmail.com>
2025-07-09 08:56:58 -0400

    Upload esercizio python

---
+++ Unit1/Settimana 2/S2L3/esercizio.py
@@ -0,0 +1,13 @@
+# Esercizio: Scrivere un programma in Python che genera un nome per una band musicale utilizzando due input forniti dall'utente:
+#  la città di origine e il nome del proprio animale domestico.
+
+def nomeBand (città, animale):
+    return f"Il nome della tua band potrebbe essere: {città} {animale}"
+
+città = input("Inserisci il nome della città: ")
+animale = input("Inserisci il nome del tuo animale preferito: ")
+
+print(nomeBand(città, animale))
+
+
+
